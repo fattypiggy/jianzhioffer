af68d689a037298178e5ed03591e5a298e3449a5
William Jing <monkeykingisback@gmail.com>
2021-08-07 15:04:18 +0800

    add Utils/ListNode

---
+++ Utils/ListNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 经纬 on 2021/8/7.
+//
+
+#include "ListNode.h"
---
+++ Utils/ListNode.h
@@ -0,0 +1,15 @@
+//
+// Created by 经纬 on 2021/8/7.
+//
+
+#ifndef 剑指OFFER_LISTNODE_H
+#define 剑指OFFER_LISTNODE_H
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+#endif //剑指OFFER_LISTNODE_H
