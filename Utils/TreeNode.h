40e699750e018550be485fd853111f759935ff52
William Jing <monkeykingisback@gmail.com>
2021-08-16 09:42:45 +0800

    update TreeNode

--- Utils/TreeNode.h
+++ Utils/TreeNode.h
@@ -1,7 +1,6 @@
 //
 // Created by 经纬 on 2021/8/7.
 //
-
 #ifndef UTILS_TREENODE_H
 #define UTILS_TREENODE_H
 
@@ -12,5 +11,9 @@ struct TreeNode {
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-
+std::string treeNodeToString(TreeNode* root);
+void trimLeftTrailingSpaces(std::string &input);
+void trimRightTrailingSpaces(std::string &input);
+void prettyPrintTree(TreeNode* node, std::string prefix, bool isLeft);
+TreeNode* stringToTreeNode(std::string input);
 #endif //UTILS_TREENODE_H
