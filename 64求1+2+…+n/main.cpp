c375f9737690c31e38abe3526166a6c9a709877b
William Jing <monkeykingisback@gmail.com>
2021-09-24 19:51:51 +0800

    add 64 求1+2+...+n

---
+++ 64求1+2+…+n/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Calculator {
+public:
+    Calculator() {
+        N++;
+        sum += N;
+    }
+    static void reset() {
+        N = 0;
+        sum = 0;
+    }
+    static unsigned int GetSum() {
+        return sum;
+    }
+private:
+    static unsigned int N;
+    static unsigned int sum;
+};
+unsigned int Calculator::sum = 0;
+unsigned int Calculator::N = 0;
+class Solution {
+public:
+    int sumNums(int n) {
+        // 若没有reset，静态成员将持续保留。
+        Calculator::reset();
+        Calculator *temp = new Calculator[n];
+        return Calculator::GetSum();
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.sumNums(5) << endl;
+    return 0;
+}
